abda95a45374c3f80184a8c9f7e0733b4f68e68f
MounishaAddala <94676193+MounishaAddala@users.noreply.github.com>
2021-11-24 16:39:01 +0530

    Create array.sh

---
+++ file.txt/array.sh
@@ -0,0 +1,18 @@
+hiiiii
+byeeee
+okay
+#!/bin/bash
+#read text file ans store it into array
+
+arr=()
+
+#read file by using whileloop
+while read -r line;
+do
+        arr+=("$(echo "$line")")
+done<file.txt
+
+for i in "${arr[@]}"
+do
+        echo "$i"
+done
